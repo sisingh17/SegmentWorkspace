a23378b19e970694f24bb4e3f96602c130cbff65
Segment Git Sync <friends@segment.com>
2025-07-17 13:40:57 +0000

    Full sync

---
+++ users/mJDZHUivraRaA9S8Hczwed.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-mJDZHUivraRaA9S8Hczwed
+  id = "mJDZHUivraRaA9S8Hczwed"
+}
+
+resource "segment_user" "id-mJDZHUivraRaA9S8Hczwed" {
+  email = "simranjeet.singh@segment.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "uyjtBM2TkQvq7X9yxWb8U5"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFMmZbyzdEWmE6k8JVgKAiE"
+    },
+  ]
+}
---
+++ users/vPUb2HfzUViokn8bQKUqbg.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_user.id-vPUb2HfzUViokn8bQKUqbg
+  id = "vPUb2HfzUViokn8bQKUqbg"
+}
+
+resource "segment_user" "id-vPUb2HfzUViokn8bQKUqbg" {
+  email = "singh.sj90@gmail.com"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "uyjtBM2TkQvq7X9yxWb8U5"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DKqCj3vrDvNml9Vhsc7Aebt"
+    },
+  ]
+}
