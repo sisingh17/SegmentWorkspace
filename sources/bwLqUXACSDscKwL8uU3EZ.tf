4a6e2aad05627d6a70e5618719d95661f8f3c20c
Segment Git Sync <friends@segment.com>
2025-07-24 15:06:15 +0000

    Source 'bwLqUXACSDscKwL8uU3EZ' created by simranjeet.singh@segment.com

---
+++ sources/bwLqUXACSDscKwL8uU3EZ.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-bwLqUXACSDscKwL8uU3EZ
+  id = "bwLqUXACSDscKwL8uU3EZ"
+}
+
+resource "segment_source" "id-bwLqUXACSDscKwL8uU3EZ" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "iUM16Md8P2"
+  }
+  name     = "Testing Demo"
+  settings = jsonencode({})
+  slug     = "testing_demo"
+}
