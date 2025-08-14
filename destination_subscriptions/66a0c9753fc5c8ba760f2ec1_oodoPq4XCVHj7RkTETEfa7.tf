5f7de642440af4bdf7a1400d03d81310d47aa05c
Segment Git Sync <friends@segment.com>
2025-08-14 09:55:18 +0000

    Destination Subscription 'oodoPq4XCVHj7RkTETEfa7' created by simranjeet.singh@segment.com

---
+++ destination_subscriptions/66a0c9753fc5c8ba760f2ec1_oodoPq4XCVHj7RkTETEfa7.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_destination_subscription.id-66a0c9753fc5c8ba760f2ec1_oodoPq4XCVHj7RkTETEfa7
+  id = "66a0c9753fc5c8ba760f2ec1:oodoPq4XCVHj7RkTETEfa7"
+}
+
+resource "segment_destination_subscription" "id-66a0c9753fc5c8ba760f2ec1_oodoPq4XCVHj7RkTETEfa7" {
+  action_id            = "nFPnRozhz1mh4Gbx4MLvT5"
+  destination_id       = "66a0c9753fc5c8ba760f2ec1"
+  enabled              = true
+  model_id             = null
+  name                 = "Audience Webhook"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    batch_keys = ["url", "method", "headers"]
+    data = {
+      "@path" = "$."
+    }
+    method = "POST"
+    url    = "https://webhook.site/7bc93cf4-8f3e-498c-8065-bf90a99f19df"
+  })
+  trigger = "event = \"Audience Entered\" or event = \"Audience Exited\""
+}
