a72ffcf0147a42bbd8b26b276dc7b9630791fa21
Segment Git Sync <friends@segment.com>
2025-08-21 15:46:18 +0000

    Destination Subscription '3ffuP9x7qA5XBE9VwWXvq5' modified by simranjeet.singh@segment.com

--- destination_subscriptions/689db62df2069c2ffa8f1110_3ffuP9x7qA5XBE9VwWXvq5.tf
+++ destination_subscriptions/689db62df2069c2ffa8f1110_3ffuP9x7qA5XBE9VwWXvq5.tf
@@ -16,7 +16,7 @@ resource "segment_destination_subscription" "id-689db62df2069c2ffa8f1110_3ffuP9x
       "@path" = "$."
     }
     method = "POST"
-    url    = "https://webhook.site/7bc93cf4-8f3e-498c-8065-bf90a99f19df"
+    url    = "https://webhook.site/e9e96e14-38e0-40bb-be9a-adb08b5866df"
   })
   trigger = "event = \"Example Event\""
 }
