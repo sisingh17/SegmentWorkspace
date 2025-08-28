433adb5ae2277a2fd137f9866eee3335f46cfde5
Segment Git Sync <friends@segment.com>
2025-08-28 12:44:52 +0000

    Reverse ETL Model '3FRWs4RZLKPSbmSgh6bSMK' created by simranjeet.singh@segment.com

---
+++ reverse_etl_models/3FRWs4RZLKPSbmSgh6bSMK.tf
@@ -0,0 +1,13 @@
+import {
+  to = segment_reverse_etl_model.id-3FRWs4RZLKPSbmSgh6bSMK
+  id = "3FRWs4RZLKPSbmSgh6bSMK"
+}
+
+resource "segment_reverse_etl_model" "id-3FRWs4RZLKPSbmSgh6bSMK" {
+  description             = " "
+  enabled                 = true
+  name                    = "RETL Test"
+  query                   = "SELECT\n  *\nFROM\n  \"SEGMENT_REVERSE_ETL\".\"__SEGMENT_REVERSE_ETL\".\"CUSTOMERS\";"
+  query_identifier_column = "user_id"
+  source_id               = "6PMPY3rtmgizcWXYW7im4g"
+}
