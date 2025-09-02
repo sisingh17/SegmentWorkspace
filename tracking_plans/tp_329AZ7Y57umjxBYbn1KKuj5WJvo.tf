1eb21a8341886c224a0d220128d271ba6af13365
Segment Git Sync <friends@segment.com>
2025-09-02 14:14:27 +0000

    Tracking Plan 'tp_329AZ7Y57umjxBYbn1KKuj5WJvo' created by simranjeet.singh@segment.com

---
+++ tracking_plans/tp_329AZ7Y57umjxBYbn1KKuj5WJvo.tf
@@ -0,0 +1,12 @@
+import {
+  to = segment_tracking_plan.id-tp_329AZ7Y57umjxBYbn1KKuj5WJvo
+  id = "tp_329AZ7Y57umjxBYbn1KKuj5WJvo"
+}
+
+resource "segment_tracking_plan" "id-tp_329AZ7Y57umjxBYbn1KKuj5WJvo" {
+  description = null
+  name        = "Mobile"
+  rules = [
+  ]
+  type = "RULE_LIBRARY"
+}
