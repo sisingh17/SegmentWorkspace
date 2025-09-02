ae135f30dd9e95a2a5aebc6c69667fdcf486bd5c
Segment Git Sync <friends@segment.com>
2025-09-02 14:18:52 +0000

    Transformation '329B6N34SpL9S0QbWlP6QBFOPQp' created by simranjeet.singh@segment.com

---
+++ transformations/329B6N34SpL9S0QbWlP6QBFOPQp.tf
@@ -0,0 +1,19 @@
+import {
+  to = segment_transformation.id-329B6N34SpL9S0QbWlP6QBFOPQp
+  id = "329B6N34SpL9S0QbWlP6QBFOPQp"
+}
+
+resource "segment_transformation" "id-329B6N34SpL9S0QbWlP6QBFOPQp" {
+  destination_metadata_id = null
+  enabled                 = true
+  fql_defined_properties = [
+  ]
+  if             = "event=\"Example Event\""
+  name           = "Test Transformation"
+  new_event_name = "New Example Event"
+  property_renames = [
+  ]
+  property_value_transformations = [
+  ]
+  source_id = "8MT9TFAJzDfUxTat4GhnxC"
+}
