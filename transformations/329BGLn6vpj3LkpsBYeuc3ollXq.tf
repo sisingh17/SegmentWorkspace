624fdef1d62ef7ce3532cbace2a6158bdb6ff89c
Segment Git Sync <friends@segment.com>
2025-09-02 14:20:11 +0000

    Transformation '329BGLn6vpj3LkpsBYeuc3ollXq' created by Public API token 'SageDemo's'

---
+++ transformations/329BGLn6vpj3LkpsBYeuc3ollXq.tf
@@ -0,0 +1,31 @@
+import {
+  to = segment_transformation.id-329BGLn6vpj3LkpsBYeuc3ollXq
+  id = "329BGLn6vpj3LkpsBYeuc3ollXq"
+}
+
+resource "segment_transformation" "id-329BGLn6vpj3LkpsBYeuc3ollXq" {
+  destination_metadata_id = null
+  enabled                 = true
+  fql_defined_properties = [
+  ]
+  if             = "event = 'Example Event V1'"
+  name           = "Name of the new transformation"
+  new_event_name = "new-event-name"
+  property_renames = [
+    {
+      new_name = "another-name-new"
+      old_name = "another-name-old"
+    },
+    {
+      new_name = "new-name"
+      old_name = "old-name"
+    },
+  ]
+  property_value_transformations = [
+    {
+      property_paths = ["context.some-property", "properties.some-property"]
+      property_value = "some property value"
+    },
+  ]
+  source_id = "8MT9TFAJzDfUxTat4GhnxC"
+}
