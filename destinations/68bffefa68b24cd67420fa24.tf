b10f5a78cabf2135ca08bfd6fd280e9bb4fca584
Segment Git Sync <friends@segment.com>
2025-09-09 10:18:36 +0000

    Destination '68bffefa68b24cd67420fa24' created by simranjeet.singh@segment.com

---
+++ destinations/68bffefa68b24cd67420fa24.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_destination.id-68bffefa68b24cd67420fa24
+  id = "68bffefa68b24cd67420fa24"
+}
+
+resource "segment_destination" "id-68bffefa68b24cd67420fa24" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "60f9d0d048950c356be2e4da"
+    partner_owned     = false
+    region_endpoints  = ["US", "EU"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name = "Braze Cloud Mode (Actions)"
+  settings = jsonencode({
+    api_key  = ""
+    app_id   = ""
+    endpoint = "https://rest.iad-01.braze.com"
+  })
+  source_id = "mGfJH44XzwfN7eHA3NdiDf"
+}
