84946f42e10c8d684778129f8f580696e3dae177
Segment Git Sync <friends@segment.com>
2025-09-09 10:05:45 +0000

    Destination '68bffbf89e7a3e912e1dede8' created by simranjeet.singh@segment.com

---
+++ destinations/68bffbf89e7a3e912e1dede8.tf
@@ -0,0 +1,20 @@
+import {
+  to = segment_destination.id-68bffbf89e7a3e912e1dede8
+  id = "68bffbf89e7a3e912e1dede8"
+}
+
+resource "segment_destination" "id-68bffbf89e7a3e912e1dede8" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "614a3c7d791c91c41bae7599"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name = "Webhooks (Actions)"
+  settings = jsonencode({
+    sharedSecret = ""
+  })
+  source_id = "mGfJH44XzwfN7eHA3NdiDf"
+}
