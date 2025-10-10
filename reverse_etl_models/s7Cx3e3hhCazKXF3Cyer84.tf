12ad817eb907e6120ff11e067380db042791100f
Segment Git Sync <friends@segment.com>
2025-10-10 10:56:38 +0000

    Reverse ETL Model 's7Cx3e3hhCazKXF3Cyer84' created by simranjeet.singh@segment.com

---
+++ reverse_etl_models/s7Cx3e3hhCazKXF3Cyer84.tf
@@ -0,0 +1,13 @@
+import {
+  to = segment_reverse_etl_model.id-s7Cx3e3hhCazKXF3Cyer84
+  id = "s7Cx3e3hhCazKXF3Cyer84"
+}
+
+resource "segment_reverse_etl_model" "id-s7Cx3e3hhCazKXF3Cyer84" {
+  description             = "Programatically generated rETL model that is associated with an Engage rETL audience"
+  enabled                 = true
+  name                    = "Linked Audience rETL Model"
+  query                   = "SELECT 1"
+  query_identifier_column = "matchId"
+  source_id               = "toMuunXPwvbTgDRsvF5a3"
+}
