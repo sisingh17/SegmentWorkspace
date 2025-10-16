a70c93c1cb39f7490cffdf38da29e8d7b8778bab
Segment Git Sync <friends@segment.com>
2025-10-16 09:21:19 +0000

    Destination '68f0b8ec5d75283166a09cd6' modified by simranjeet.singh@segment.com

--- destinations/68f0b8ec5d75283166a09cd6.tf
+++ destinations/68f0b8ec5d75283166a09cd6.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-68f0b8ec5d75283166a09cd6" {
-  enabled = false
+  enabled = true
   metadata = {
     contacts          = null
     id                = "5a6b50f1c900fa00011858fd"
@@ -14,10 +14,11 @@ resource "segment_destination" "id-68f0b8ec5d75283166a09cd6" {
   }
   name = "Google Ads Remarketing Lists"
   settings = jsonencode({
-    account           = ""
-    adPersonalization = ""
-    adUserData        = ""
-    appId             = ""
+    account             = "2313397280"
+    accountResourceName = "customers/2313397280/customerClients/2313397280"
+    adPersonalization   = ""
+    adUserData          = ""
+    appId               = ""
   })
   source_id = "mGfJH44XzwfN7eHA3NdiDf"
 }
