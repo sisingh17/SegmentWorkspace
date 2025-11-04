9abd51a609a479d365ce3add6e8381a9eacbdfac
Segment Git Sync <friends@segment.com>
2025-11-04 11:49:29 +0000

    Destination '6909e84856b8ab698f4c9016' created by simranjeet.singh@segment.com

---
+++ destinations/6909e84856b8ab698f4c9016.tf
@@ -0,0 +1,26 @@
+import {
+  to = segment_destination.id-6909e84856b8ab698f4c9016
+  id = "6909e84856b8ab698f4c9016"
+}
+
+resource "segment_destination" "id-6909e84856b8ab698f4c9016" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "54521fd925e721e32a72eee0"
+    partner_owned     = false
+    region_endpoints  = ["EU", "US"]
+    supported_regions = null
+  }
+  name = "New Relic Test"
+  settings = jsonencode({
+    accountId        = ""
+    defaultEventType = ""
+    events           = {}
+    insertKey        = ""
+    region           = ""
+    sendContext      = false
+    sendIds          = false
+  })
+  source_id = "8MT9TFAJzDfUxTat4GhnxC"
+}
