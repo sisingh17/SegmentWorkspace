96c57152235182b833994730e1aae473704067b8
Segment Git Sync <friends@segment.com>
2025-11-24 09:34:52 +0000

    Destination Subscription '7FuNNFURpuEx5qEDHgxXif' modified by simranjeet.singh@segment.com

--- destination_subscriptions/68e8e6f74e9fcf0225ced7e0_7FuNNFURpuEx5qEDHgxXif.tf
+++ destination_subscriptions/68e8e6f74e9fcf0225ced7e0_7FuNNFURpuEx5qEDHgxXif.tf
@@ -6,13 +6,15 @@ import {
 resource "segment_destination_subscription" "id-68e8e6f74e9fcf0225ced7e0_7FuNNFURpuEx5qEDHgxXif" {
   action_id            = "3MxuLcLqv1XvQS8iWuX3xZ"
   destination_id       = "68e8e6f74e9fcf0225ced7e0"
-  enabled              = false
+  enabled              = true
   model_id             = null
   name                 = "Customer Match User List"
   reverse_etl_schedule = null
   settings = jsonencode({
-    __segment_internal_sync_mode = "add"
-    batch_size                   = 10000
+    __segment_internal_sync_mode     = "add"
+    ad_personalization_consent_state = "GRANTED"
+    ad_user_data_consent_state       = "GRANTED"
+    batch_size                       = 10000
     email = {
       "@if" = {
         else = {
