a8c240306bedff133baade02fc45c5bba1bb339d
Segment Git Sync <friends@segment.com>
2025-12-10 09:45:46 +0000

    Destination '69394149f28c4a7c390d25e0' created by simranjeet.singh@segment.com

---
+++ destinations/69394149f28c4a7c390d25e0.tf
@@ -0,0 +1,20 @@
+import {
+  to = segment_destination.id-69394149f28c4a7c390d25e0
+  id = "69394149f28c4a7c390d25e0"
+}
+
+resource "segment_destination" "id-69394149f28c4a7c390d25e0" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "614a3c7d791c91c41bae7599"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name = "Test Relay"
+  settings = jsonencode({
+    sharedSecret = ""
+  })
+  source_id = "8MT9TFAJzDfUxTat4GhnxC"
+}
