726d25bcf3ef497805f1672874935667e4d424ad
Segment Git Sync <friends@segment.com>
2026-04-13 09:37:39 +0000

    Full sync

---
+++ labels/environment_demo.tf
@@ -0,0 +1,10 @@
+import {
+  to = segment_label.id-environment_demo
+  id = "environment:demo"
+}
+
+resource "segment_label" "id-environment_demo" {
+  description = null
+  key         = "environment"
+  value       = "demo"
+}
