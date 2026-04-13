037b1efa6b5509399385e69f7ea05eacbe44e761
Segment Git Sync <friends@segment.com>
2026-04-13 09:37:41 +0000

    Full sync

---
+++ warehouses/e8HL34FtoxXYmttmLDjYHk.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_warehouse.id-e8HL34FtoxXYmttmLDjYHk
+  id = "e8HL34FtoxXYmttmLDjYHk"
+}
+
+resource "segment_warehouse" "id-e8HL34FtoxXYmttmLDjYHk" {
+  enabled = true
+  metadata = {
+    id = "75JTYATfQJSmDmcuLotsz5"
+  }
+  name = "new Databricks"
+  settings = jsonencode({
+    credential_id = "3ARCZAVzEm1OMMUlZzT62M4LKbM"
+  })
+}
