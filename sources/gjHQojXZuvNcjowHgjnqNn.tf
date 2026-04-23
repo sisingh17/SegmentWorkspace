7e0fdf30784c94b9e4af28b35c29f4fee3503b1d
Segment Git Sync <friends@segment.com>
2026-04-23 10:28:53 +0000

    Source 'gjHQojXZuvNcjowHgjnqNn' created by simranjeet.singh@segment.com

---
+++ sources/gjHQojXZuvNcjowHgjnqNn.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-gjHQojXZuvNcjowHgjnqNn
+  id = "gjHQojXZuvNcjowHgjnqNn"
+}
+
+resource "segment_source" "id-gjHQojXZuvNcjowHgjnqNn" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "iUM16Md8P2"
+  }
+  name     = "Journey_to_onesignal"
+  settings = jsonencode({})
+  slug     = "journey_to_onesignal"
+}
