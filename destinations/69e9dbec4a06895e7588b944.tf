4443b95d760cf07d3bb5f1b23e30369a8d8a7d3f
Segment Git Sync <friends@segment.com>
2026-04-23 08:44:29 +0000

    Destination '69e9dbec4a06895e7588b944' created by simranjeet.singh@segment.com

---
+++ destinations/69e9dbec4a06895e7588b944.tf
@@ -0,0 +1,24 @@
+import {
+  to = segment_destination.id-69e9dbec4a06895e7588b944
+  id = "69e9dbec4a06895e7588b944"
+}
+
+resource "segment_destination" "id-69e9dbec4a06895e7588b944" {
+  enabled = false
+  metadata = {
+    contacts = [
+      {
+      },
+    ]
+    id                = "60b6a5b69fec493efbd3c64c"
+    partner_owned     = true
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name = "OneSignal (New)"
+  settings = jsonencode({
+    apiKey = ""
+    appId  = ""
+  })
+  source_id = "mGfJH44XzwfN7eHA3NdiDf"
+}
