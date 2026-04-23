d2864e01e60dfbc78beb78c935ed1e800563212e
Segment Git Sync <friends@segment.com>
2026-04-23 10:19:40 +0000

    Destination '69e9f23b89da668c668be94f' created by simranjeet.singh@segment.com

---
+++ destinations/69e9f23b89da668c668be94f.tf
@@ -0,0 +1,24 @@
+import {
+  to = segment_destination.id-69e9f23b89da668c668be94f
+  id = "69e9f23b89da668c668be94f"
+}
+
+resource "segment_destination" "id-69e9f23b89da668c668be94f" {
+  enabled = false
+  metadata = {
+    contacts = [
+      {
+      },
+    ]
+    id                = "60b6a5b69fec493efbd3c64c"
+    partner_owned     = true
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name = "OneSignalJourney Test"
+  settings = jsonencode({
+    apiKey = "••••••••••4acy"
+    appId  = "b1af2d84-6dfd-4fd3-af8c-646b4773900c"
+  })
+  source_id = "tFVMmgJLfeddhwQYLCjGJu"
+}
