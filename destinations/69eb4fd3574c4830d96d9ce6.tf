0b1dfa2e1a99d2b9bd53a1bd74973329b78c8c96
Segment Git Sync <friends@segment.com>
2026-04-24 11:11:17 +0000

    Destination '69eb4fd3574c4830d96d9ce6' created by simranjeet.singh@segment.com

---
+++ destinations/69eb4fd3574c4830d96d9ce6.tf
@@ -0,0 +1,24 @@
+import {
+  to = segment_destination.id-69eb4fd3574c4830d96d9ce6
+  id = "69eb4fd3574c4830d96d9ce6"
+}
+
+resource "segment_destination" "id-69eb4fd3574c4830d96d9ce6" {
+  enabled = false
+  metadata = {
+    contacts = [
+      {
+      },
+    ]
+    id                = "60b6a5b69fec493efbd3c64c"
+    partner_owned     = true
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name = "Journey-HTTP_API-1Signal"
+  settings = jsonencode({
+    apiKey = "••••••••••4acy"
+    appId  = "b1af2d84-6dfd-4fd3-af8c-646b4773900c"
+  })
+  source_id = "gjHQojXZuvNcjowHgjnqNn"
+}
