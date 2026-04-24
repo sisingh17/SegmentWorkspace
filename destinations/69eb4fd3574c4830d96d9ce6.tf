ddaa5495a850c8dee812fd1387fb70b19a6e7c7c
Segment Git Sync <friends@segment.com>
2026-04-24 11:29:33 +0000

    Destination '69eb4fd3574c4830d96d9ce6' modified by simranjeet.singh@segment.com

--- destinations/69eb4fd3574c4830d96d9ce6.tf
+++ destinations/69eb4fd3574c4830d96d9ce6.tf
@@ -17,7 +17,7 @@ resource "segment_destination" "id-69eb4fd3574c4830d96d9ce6" {
   }
   name = "Journey-HTTP_API-1Signal"
   settings = jsonencode({
-    apiKey = "••••••••••4acy"
+    apiKey = "••••••••••i7fq"
     appId  = "b1af2d84-6dfd-4fd3-af8c-646b4773900c"
   })
   source_id = "gjHQojXZuvNcjowHgjnqNn"
