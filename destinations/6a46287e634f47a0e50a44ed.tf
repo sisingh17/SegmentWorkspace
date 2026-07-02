681e0a1fedfef0e71625a8a18f31d652c776d379
Segment Git Sync <friends@segment.com>
2026-07-02 09:00:27 +0000

    Destination '6a46287e634f47a0e50a44ed' modified by simranjeet.singh@segment.com

--- destinations/6a46287e634f47a0e50a44ed.tf
+++ destinations/6a46287e634f47a0e50a44ed.tf
@@ -4,7 +4,7 @@ import {
 }
 
 resource "segment_destination" "id-6a46287e634f47a0e50a44ed" {
-  enabled = false
+  enabled = true
   metadata = {
     contacts          = null
     id                = "5a6b50f1c900fa00011858fd"
@@ -14,10 +14,11 @@ resource "segment_destination" "id-6a46287e634f47a0e50a44ed" {
   }
   name = "Google Ads Remarketing Lists (2)"
   settings = jsonencode({
-    account           = ""
-    adPersonalization = ""
-    adUserData        = ""
-    appId             = ""
+    account             = "2313397280"
+    accountResourceName = "customers/2313397280/customerClients/2313397280"
+    adPersonalization   = ""
+    adUserData          = ""
+    appId               = ""
   })
   source_id = "hWwGoDVBrvVXXEcSMAqJSX"
 }
