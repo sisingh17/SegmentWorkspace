1430c835bae0ec5c31e49633d0d317167caeeeda
Segment Git Sync <friends@segment.com>
2026-07-02 15:10:42 +0000

    Destination '6a467f71cc8b1c5811a4f48e' created by simranjeet.singh@segment.com

---
+++ destinations/6a467f71cc8b1c5811a4f48e.tf
@@ -0,0 +1,20 @@
+import {
+  to = segment_destination.id-6a467f71cc8b1c5811a4f48e
+  id = "6a467f71cc8b1c5811a4f48e"
+}
+
+resource "segment_destination" "id-6a467f71cc8b1c5811a4f48e" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "6a467f398821c242726df2d4"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name = "Vero Destination Function (Simran Internal)"
+  settings = jsonencode({
+    veroAuthToken = ""
+  })
+  source_id = "hWwGoDVBrvVXXEcSMAqJSX"
+}
