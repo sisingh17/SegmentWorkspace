44202b048af022f238ef5372ebe4925f7f3de1e2
Segment Git Sync <friends@segment.com>
2026-07-29 14:15:34 +0000

    Destination Subscription 'bedNUmM2tC7TzhgjQT6BzN' created by simranjeet.singh@segment.com

---
+++ destination_subscriptions/6a6a0a145b92abdd53df3a6c_bedNUmM2tC7TzhgjQT6BzN.tf
@@ -0,0 +1,91 @@
+import {
+  to = segment_destination_subscription.id-6a6a0a145b92abdd53df3a6c_bedNUmM2tC7TzhgjQT6BzN
+  id = "6a6a0a145b92abdd53df3a6c:bedNUmM2tC7TzhgjQT6BzN"
+}
+
+resource "segment_destination_subscription" "id-6a6a0a145b92abdd53df3a6c_bedNUmM2tC7TzhgjQT6BzN" {
+  action_id            = "i9V1H1emvbxxNuwB5bdALx"
+  destination_id       = "6a6a0a145b92abdd53df3a6c"
+  enabled              = false
+  model_id             = null
+  name                 = "Sync To LinkedIn DMP Segment"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    company = {
+      "@path" = "$.traits.company"
+    }
+    country = {
+      "@path" = "$.traits.country"
+    }
+    dmp_segment_name = {
+      "@if" = {
+        else = {
+          "@path" = "$.context.personas.computation_key"
+        }
+        exists = {
+          "@path" = "$.properties.audience_key"
+        }
+        then = {
+          "@path" = "$.properties.audience_key"
+        }
+      }
+    }
+    dmp_user_action = "AUTO"
+    email = {
+      "@if" = {
+        else = {
+          "@path" = "$.traits.email"
+        }
+        exists = {
+          "@path" = "$.context.traits.email"
+        }
+        then = {
+          "@path" = "$.context.traits.email"
+        }
+      }
+    }
+    enable_batching = true
+    event_name = {
+      "@path" = "$.event"
+    }
+    first_name = {
+      "@path" = "$.traits.firstName"
+    }
+    google_advertising_id = {
+      "@path" = "$.context.device.advertisingId"
+    }
+    last_name = {
+      "@path" = "$.traits.lastName"
+    }
+    personas_audience_key = {
+      "@if" = {
+        else = {
+          "@path" = "$.context.personas.computation_key"
+        }
+        exists = {
+          "@path" = "$.properties.audience_key"
+        }
+        then = {
+          "@path" = "$.properties.audience_key"
+        }
+      }
+    }
+    source_segment_id = {
+      "@if" = {
+        else = {
+          "@path" = "$.context.personas.computation_key"
+        }
+        exists = {
+          "@path" = "$.properties.audience_key"
+        }
+        then = {
+          "@path" = "$.properties.audience_key"
+        }
+      }
+    }
+    title = {
+      "@path" = "$.traits.title"
+    }
+  })
+  trigger = "event = \"Audience Entered\" or event = \"Audience Exited\""
+}
