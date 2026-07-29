0f9412942d97fa217ed0a68fbca67de9c5c610b2
Segment Git Sync <friends@segment.com>
2026-07-29 14:11:34 +0000

    Destination '6a6a0a145b92abdd53df3a6c' created by simranjeet.singh@segment.com

---
+++ destinations/6a6a0a145b92abdd53df3a6c.tf
@@ -0,0 +1,27 @@
+import {
+  to = segment_destination.id-6a6a0a145b92abdd53df3a6c
+  id = "6a6a0a145b92abdd53df3a6c"
+}
+
+resource "segment_destination" "id-6a6a0a145b92abdd53df3a6c" {
+  enabled = false
+  metadata = {
+    contacts = [
+      {
+      },
+      {
+      },
+    ]
+    id                = "62f435d1d311567bd5bf0e8d"
+    partner_owned     = false
+    region_endpoints  = ["US"]
+    supported_regions = ["us-west-2", "eu-west-1"]
+  }
+  name = "LinkedIn Audiences"
+  settings = jsonencode({
+    ad_account_id              = ""
+    send_email                 = true
+    send_google_advertising_id = true
+  })
+  source_id = "csDfmZ7LdFdHnFMVyFWbHX"
+}
