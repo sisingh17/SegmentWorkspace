c0c460ae2507d7f20fb3b93e03d6ed9bfc1d2bd1
Segment Git Sync <friends@segment.com>
2026-07-30 13:48:21 +0000

    Destination '6a6b562307b6022b0284430f' created by simranjeet.singh@segment.com

---
+++ destinations/6a6b562307b6022b0284430f.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_destination.id-6a6b562307b6022b0284430f
+  id = "6a6b562307b6022b0284430f"
+}
+
+resource "segment_destination" "id-6a6b562307b6022b0284430f" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "5a4d24dcc5836400017188f6"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name = "Personas Facebook Custom Audiences (2)"
+  settings = jsonencode({
+    account           = ""
+    specialAdCategory = ""
+  })
+  source_id = "kaXUTfhJT5J2NqHvd5JGar"
+}
